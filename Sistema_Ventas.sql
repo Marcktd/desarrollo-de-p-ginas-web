01e33b8198484c66a5257252932f1f82238978da
Marcktd <85458136+Marcktd@users.noreply.github.com>
2026-06-27 17:23:57 +0000

    Entrega final: Código, SQL y README completados

--- Sistema_Ventas.sql
+++ Sistema_Ventas.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-06-2026 a las 23:53:36
+-- Servidor: localhost
+-- Tiempo de generación: 27-06-2026 a las 19:20:13
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,18 +18,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sistema_ventas`
+-- Base de datos: `Sistema_Ventas`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `apertura_caja`
+-- Estructura de tabla para la tabla `Apertura_caja`
 --
 
-CREATE TABLE `apertura_caja` (
+CREATE TABLE `Apertura_caja` (
   `id` int(11) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
+  `is_usuario` int(11) NOT NULL,
   `fecha` datetime NOT NULL DEFAULT current_timestamp(),
   `monto_inicial` decimal(10,2) NOT NULL,
   `estado` enum('abierto','cerrado','en proceso','pendiente') NOT NULL
@@ -57,10 +57,10 @@ INSERT INTO `categoria` (`id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cliente`
+-- Estructura de tabla para la tabla `Cliente`
 --
 
-CREATE TABLE `cliente` (
+CREATE TABLE `Cliente` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `rnc_cedula` varchar(20) NOT NULL,
@@ -69,19 +69,19 @@ CREATE TABLE `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `cliente`
+-- Volcado de datos para la tabla `Cliente`
 --
 
-INSERT INTO `cliente` (`id`, `nombre`, `rnc_cedula`, `dirección`, `telefono`) VALUES
+INSERT INTO `Cliente` (`id`, `nombre`, `rnc_cedula`, `dirección`, `telefono`) VALUES
 (1, 'MIchael', 'na', '', 'na');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `detalle_venta`
+-- Estructura de tabla para la tabla `Detalle_venta`
 --
 
-CREATE TABLE `detalle_venta` (
+CREATE TABLE `Detalle_venta` (
   `id` int(11) NOT NULL,
   `id_venta` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
@@ -92,10 +92,10 @@ CREATE TABLE `detalle_venta` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `producto`
+-- Estructura de tabla para la tabla `Producto`
 --
 
-CREATE TABLE `producto` (
+CREATE TABLE `Producto` (
   `id` int(11) NOT NULL,
   `codigo` varchar(50) NOT NULL COMMENT 'EL codigo de barras de los productos',
   `nombre` varchar(100) NOT NULL,
@@ -105,19 +105,19 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `producto`
+-- Volcado de datos para la tabla `Producto`
 --
 
-INSERT INTO `producto` (`id`, `codigo`, `nombre`, `precio`, `stock`, `id_categoria`) VALUES
+INSERT INTO `Producto` (`id`, `codigo`, `nombre`, `precio`, `stock`, `id_categoria`) VALUES
 (1, '', 'Computadora lexus', 22000.00, 5, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `proveedor`
+-- Estructura de tabla para la tabla `Proveedor`
 --
 
-CREATE TABLE `proveedor` (
+CREATE TABLE `Proveedor` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `telefono` varchar(20) NOT NULL,
@@ -126,19 +126,19 @@ CREATE TABLE `proveedor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `proveedor`
+-- Volcado de datos para la tabla `Proveedor`
 --
 
-INSERT INTO `proveedor` (`id`, `nombre`, `telefono`, `direccion`, `rnc`) VALUES
+INSERT INTO `Proveedor` (`id`, `nombre`, `telefono`, `direccion`, `rnc`) VALUES
 (1, 'Esteban', '', '', '2-2222222-2');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `Usuario`
 --
 
-CREATE TABLE `usuario` (
+CREATE TABLE `Usuario` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
@@ -146,10 +146,10 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Volcado de datos para la tabla `Usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nombre`, `correo`, `password`) VALUES
+INSERT INTO `Usuario` (`id`, `nombre`, `correo`, `password`) VALUES
 (1, '', 'marcospenarosario@gmail.com\r\n', '1234'),
 (2, 'Administrador', 'admin@tienda.com', '12345'),
 (3, 'Admin', 'admin@tienda.com', '12345'),
@@ -158,15 +158,14 @@ INSERT INTO `usuario` (`id`, `nombre`, `correo`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `venta`
+-- Estructura de tabla para la tabla `Venta`
 --
 
-CREATE TABLE `venta` (
+CREATE TABLE `Venta` (
   `id` int(11) NOT NULL,
   `total` decimal(10,0) NOT NULL,
   `fecha` datetime NOT NULL DEFAULT current_timestamp(),
   `id_cliente` int(11) NOT NULL,
-  `id_apertura_caja` int(11) DEFAULT NULL,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -175,11 +174,10 @@ CREATE TABLE `venta` (
 --
 
 --
--- Indices de la tabla `apertura_caja`
+-- Indices de la tabla `Apertura_caja`
 --
-ALTER TABLE `apertura_caja`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_caja_usuario` (`id_usuario`);
+ALTER TABLE `Apertura_caja`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `categoria`
@@ -188,55 +186,54 @@ ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `cliente`
+-- Indices de la tabla `Cliente`
 --
-ALTER TABLE `cliente`
+ALTER TABLE `Cliente`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `detalle_venta`
+-- Indices de la tabla `Detalle_venta`
 --
-ALTER TABLE `detalle_venta`
+ALTER TABLE `Detalle_venta`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_detaventa_venta` (`id_venta`),
   ADD KEY `fk_detaventa_producto` (`id_producto`);
 
 --
--- Indices de la tabla `producto`
+-- Indices de la tabla `Producto`
 --
-ALTER TABLE `producto`
+ALTER TABLE `Producto`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_producto_categoria` (`id_categoria`);
 
 --
--- Indices de la tabla `proveedor`
+-- Indices de la tabla `Proveedor`
 --
-ALTER TABLE `proveedor`
+ALTER TABLE `Proveedor`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `usuario`
+-- Indices de la tabla `Usuario`
 --
-ALTER TABLE `usuario`
+ALTER TABLE `Usuario`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `venta`
+-- Indices de la tabla `Venta`
 --
-ALTER TABLE `venta`
+ALTER TABLE `Venta`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_venta_usuario` (`id_cliente`),
-  ADD KEY `fk_venta_cliente` (`id_usuario`),
-  ADD KEY `fk_venta_caja` (`id_apertura_caja`);
+  ADD KEY `fk_venta_cliente` (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `apertura_caja`
+-- AUTO_INCREMENT de la tabla `Apertura_caja`
 --
-ALTER TABLE `apertura_caja`
+ALTER TABLE `Apertura_caja`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -246,39 +243,39 @@ ALTER TABLE `categoria`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `cliente`
+-- AUTO_INCREMENT de la tabla `Cliente`
 --
-ALTER TABLE `cliente`
+ALTER TABLE `Cliente`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `detalle_venta`
+-- AUTO_INCREMENT de la tabla `Detalle_venta`
 --
-ALTER TABLE `detalle_venta`
+ALTER TABLE `Detalle_venta`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `producto`
+-- AUTO_INCREMENT de la tabla `Producto`
 --
-ALTER TABLE `producto`
+ALTER TABLE `Producto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `proveedor`
+-- AUTO_INCREMENT de la tabla `Proveedor`
 --
-ALTER TABLE `proveedor`
+ALTER TABLE `Proveedor`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `usuario`
+-- AUTO_INCREMENT de la tabla `Usuario`
 --
-ALTER TABLE `usuario`
+ALTER TABLE `Usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `venta`
+-- AUTO_INCREMENT de la tabla `Venta`
 --
-ALTER TABLE `venta`
+ALTER TABLE `Venta`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -286,31 +283,24 @@ ALTER TABLE `venta`
 --
 
 --
--- Filtros para la tabla `apertura_caja`
+-- Filtros para la tabla `Detalle_venta`
 --
-ALTER TABLE `apertura_caja`
-  ADD CONSTRAINT `fk_caja_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+ALTER TABLE `Detalle_venta`
+  ADD CONSTRAINT `fk_detaventa_producto` FOREIGN KEY (`id_producto`) REFERENCES `Producto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_detaventa_venta` FOREIGN KEY (`id_venta`) REFERENCES `Venta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `detalle_venta`
+-- Filtros para la tabla `Producto`
 --
-ALTER TABLE `detalle_venta`
-  ADD CONSTRAINT `fk_detaventa_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_detaventa_venta` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
+ALTER TABLE `Producto`
   ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `venta`
+-- Filtros para la tabla `Venta`
 --
-ALTER TABLE `venta`
-  ADD CONSTRAINT `fk_venta_caja` FOREIGN KEY (`id_apertura_caja`) REFERENCES `apertura_caja` (`id`),
-  ADD CONSTRAINT `fk_venta_cliente` FOREIGN KEY (`id_usuario`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_venta_usuario` FOREIGN KEY (`id_cliente`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Venta`
+  ADD CONSTRAINT `fk_venta_cliente` FOREIGN KEY (`id_usuario`) REFERENCES `Cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_venta_usuario` FOREIGN KEY (`id_cliente`) REFERENCES `Usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
