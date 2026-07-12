421a06feab36c7052678234823aac9afcbcf8821
Agustin Ventura <agustinventuram12@gmail.com>
2026-07-12 23:29:52 +0000

    Fix: Ajuste de credenciales en conexion.php y limpieza de salidas de texto

--- conexion.php
+++ conexion.php
@@ -2,12 +2,11 @@
 $host = 'localhost';
 $usuario = 'root';
 $contraseña = '';
-$base_de_datos = 'Sistema_Ventas';
+$base_de_datos = 'sistema_ventas';
 
 $conexion = mysqli_connect($host, $usuario, $contraseña, $base_de_datos);
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
-echo "Conexión exitosa";
 ?>
